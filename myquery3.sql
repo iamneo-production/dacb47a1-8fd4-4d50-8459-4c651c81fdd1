860da49ce97755e175c9e6846c616a1715dee942
Gangina-Amani <ganginaamani999@gmail.com>
2023-07-11 08:26:22 +0000

    commit

--- myquery3.sql
+++ myquery3.sql
@@ -3,6 +3,11 @@ SELECT CAND_NAME
 FROM election
 WHERE ST_NAME = 'Andaman & Nicobar Islands' AND PARTYABBRE = 'BJP' AND YEAR = 1984;
 
+-- BJP candidate list participated at state Andhra Pradesh in the year 1984
+SELECT CAND_NAME
+FROM election
+WHERE ST_NAME = 'Andhra Pradesh' AND PARTYABBRE = 'BJP' AND YEAR = 1984;
+
 --  How many Male Candidates participated in elections in each state
 SELECT ST_NAME, COUNT(DISTINCT CAND_NAME)
 FROM election
@@ -19,8 +24,19 @@ SELECT COUNT(DISTINCT CAND_NAME)
 FROM election
 WHERE ST_NAME = 'West Bengal' AND YEAR = 1988;
 
+--  How many candidates participated in the year Assam 1998
+SELECT COUNT(DISTINCT CAND_NAME)
+FROM election
+WHERE ST_NAME = 'Assam' AND YEAR = 1998;
+
 --  Display the Tamilnadu Candidate list each year
 SELECT YEAR, CAND_NAME
 FROM election
 WHERE ST_NAME = 'Tamilnadu'
+ORDER BY YEAR;
+
+--  Display the Bihar Candidate list each year
+SELECT YEAR, CAND_NAME
+FROM election
+WHERE ST_NAME = 'Bihar'
 ORDER BY YEAR;
