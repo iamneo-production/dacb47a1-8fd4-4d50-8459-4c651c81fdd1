b7d509df390448b5cca52b052922572d5173cfd2
Harsha10102001 <harsha10102001@gmail.com>
2023-07-11 10:16:05 +0000

    TUNING_QUERIES_COMMITED

--- Election_table_queries_Harsha.sql
+++ Election_table_queries_Harsha.sql
@@ -86,9 +86,76 @@
 ------------------------------**************Performance tuning *************-----------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
---6. To find the total votes BJP got in each state in the year 1987.
-     
+--6. To find the total votes BJP got in each state in the year 1989.
+     select sum(TOTVOTPOLL) AS TOTAL_VOTES,st_name
+     from ELECTION
+     where year=1989 and PARTYABBRE='BJP'
+     group by st_name;
 
+
+--7.To find the total votes BJP got in the state 'ANDHRA PRADESH' in the year 1984.
+    select sum(TOTVOTPOLL) as Total_votes
+    from ELECTION
+    where st_name='Andhra Pradesh' and year=1984
+    group by st_name;
+
+--8. Find the Total vote percentage of BJP get in AP state in the year 2004.
+      select (sum(TOTVOTPOLL)/sum(electors))*100 as vote_percentage
+      from ELECTION
+      where st_name='Andhra Pradesh' and year=2004;    
+
+--9. How many times has BJP gotten an above 50% votes.
+     select count(*) as count_above_50
+     from ELECTION
+     where PARTYNAME='BJP' and ((TOTVOTPOLL/electors)*100)<50;
+    
+  
+--10.  what is the state list the BJP gets below 75% vote.
+    select distinct(st_name) as state_list
+    from election
+    where partyname='BJP' and  ((totvotpoll/ELECTORS)*100)<75; 
+
+
+--11. find the total candidates who participated in Assam state in 2014.
+      select count(CAND_NAME) as TOTAL_CANDIDATES
+      from ELECTION
+      where st_name='Assam' and year=2014;
+
+----------------------------------------------------------------------------------------------------------------
+------------------------------------*************performance tuning set2 ***********----------------------------
+--------------------------------------------------------------------------------------------------------------------
+
+--1.   Display the candidates list participated at state Andaman & nicobar islands in the year 1984.
+    select cand_name
+    from election
+    where st_name='Andaman & Nicobar islands' and year=1984 and PARTYNAME='BJP';      
+
+--  2.How many Male candidates participated in elections in each state.
+    select  count(*),ST_NAME
+    from ELECTION
+    where CAND_SEX='M'
+    group by st_name;    
+
+--  3. Display the party name in descending order.
+     select distinct(PARTYNAME)
+     from ELECTION
+     order by partyname desc;         
+
+-- 4. Display the Bihar candidates list each year.
+    select distinct(cand_name),year
+    from election
+    where st_name='Bihar'
+    order by year desc;    
+
+--5.  Display how many candidates participated at state Assam in the year 1984.    
+     select count(cand_name) as candidates_count
+    from election
+    where st_name='Assam' and year=1984;
+
+
+-----------------------------------------------------------------------------------------------------------------
+------------------------------**************Performance tuning set 3***************------------------------------
+-----------------------------------------------------------------------------------------------------------------
 
 
 --1. To calculate Total votes got BJP in bihar state in 1996.
@@ -97,4 +164,17 @@ from ELECTION
 where st_name='Bihar' and year=1996 and PARTYABBRE='BJP'
 group by st_name,year;
 
---2. To display the BJP candidates 
+--2. To display the BJP candidates list in descending order.
+      select distinct(cand_name)
+    from election
+    where partyabbre='BJP'
+    ORDER by CAND_NAME desc; 
+
+--3. Find how many times congress gets above 50% vote in bihar state.
+     select count(*)
+     from election
+     where PARTYABBRE='INC' and st_name='Bihar' and ((totvotpoll/ELECTORS)*100)<50;
+  
+
+
+
