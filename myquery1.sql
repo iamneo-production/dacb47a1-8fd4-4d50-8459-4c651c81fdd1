6b89da32c6abccea80bad83e03cbe660fcdfd33b
Gangina-Amani <ganginaamani999@gmail.com>
2023-07-11 11:47:02 +0000

    commitmyquery1

--- myquery1.sql
+++ myquery1.sql
@@ -87,7 +87,92 @@ FROM (
   WHERE PARTYABBRE = 'BJP'
   GROUP BY ST_NAME, YEAR
 )  SUBQUERY
-WHERE BJP_VOTE_PERCENT < 75;
+whERE BJP_VOTE_PERCENT < 75;
+
+-- BJP candidate list participated at state Andaman & Nicobar Islands in the year 1984
+SELECT CAND_NAME
+FROM election
+WHERE ST_NAME = 'Andaman & Nicobar Islands' AND PARTYABBRE = 'BJP' AND YEAR = 1984;
+
+-- BJP candidate list participated at state Andhra Pradesh in the year 1984
+SELECT CAND_NAME
+FROM election
+WHERE ST_NAME = 'Andhra Pradesh' AND PARTYABBRE = 'BJP' AND YEAR = 1984;
+
+--  How many Male Candidates participated in elections in each state
+SELECT ST_NAME, COUNT(DISTINCT CAND_NAME)
+FROM election
+WHERE CAND_SEX = 'M'
+GROUP BY ST_NAME;
+
+--  Display the Party name in descending order
+SELECT DISTINCT PARTYNAME
+FROM election
+ORDER BY PARTYNAME DESC;
+
+--  How many candidates participated in the year West Bengal 1988
+SELECT COUNT(DISTINCT CAND_NAME)
+FROM election
+WHERE ST_NAME = 'West Bengal' AND YEAR = 1988;
+
+--  How many candidates participated in the year Assam 1998
+SELECT COUNT(DISTINCT CAND_NAME)
+FROM election
+WHERE ST_NAME = 'Assam' AND YEAR = 1998;
+
+--  Display the Tamilnadu Candidate list each year
+SELECT YEAR, CAND_NAME
+FROM election
+WHERE ST_NAME = 'Tamilnadu'
+ORDER BY YEAR;
+
+--  Display the Bihar Candidate list each year
+SELECT YEAR, CAND_NAME
+FROM election
+WHERE ST_NAME = 'Bihar'
+ORDER BY YEAR;
+
+--  Total votes got BJP in Bihar State in 1996
+SELECT SUM(TOTVOTPOLL) AS "TOTAL_VOTES"
+FROM election
+WHERE ST_NAME = 'Bihar' AND PARTYABBRE = 'BJP' AND 
+YEAR = 1996;
+
+
+--  BJP Candidates list in descending order
+SELECT CAND_NAME
+FROM election
+WHERE PARTYABBRE = 'BJP'
+ORDER BY CAND_NAME DESC;
+
+
+--  Number of times Congress gets below 50% vote in Bihar state
+SELECT COUNT(*)
+FROM (
+  SELECT YEAR, SUM(TOTVOTPOLL)/(SELECT SUM(TOTVOTPOLL) FROM election WHERE ST_NAME = 'Bihar' AND YEAR = election.YEAR) * 100 AS "CONGRESS_VOTE_PERCENT"
+  FROM election
+  WHERE ST_NAME = 'Bihar' AND PARTYABBRE = 'INC'
+  GROUP BY YEAR
+) SUBQUERY
+WHERE "CONGRESS_VOTE_PERCENT" < 50;
+
+
+--  Assam Candidate list each year
+SELECT YEAR, CAND_NAME
+FROM election
+WHERE ST_NAME = 'Assam'
+ORDER BY YEAR;
+
+
+--  Total male candidates who participated in the election
+SELECT COUNT(DISTINCT CAND_NAME)
+FROM election
+WHERE "CAND_SEX" = 'M';
+
+
+
+
+
 
 
 
