4a17f60a60a06ecd12e91aa427ad4e68b5b9b1b2
gnaneswariec <gnaneswariec@gmail.com>
2023-07-11 13:50:12 +0000

    queries commited

--- .github/workflows/Election_Table_QUERIES_Gnaneswari_EC.sql
+++ .github/workflows/Election_Table_QUERIES_Gnaneswari_EC.sql
@@ -56,3 +56,42 @@ where slno<6;
 
 
 #--------------frequent queries on performance tuning--------------------#
+
+-----------query to display BJP candidates in descending order---------
+select cand_name
+from election
+where partyname='BJP' order by  cand_name desc;
+
+-----------query to display total votes got by congress party in assam in each year-----------------
+select count(TOTVOTPOLL)
+from election where partyname='INC'
+and st_name='Assam'
+group by YEAR;
+
+------------query to display election details of assam state-----------------
+select * from election where st_name='Assam';
+
+------------query to display total male candidates participated in election--------------
+select count(cand_sex) 
+from election
+where cand_sex='M';
+
+------------query to display BJP candidates at andaman &nicobar in the year 1984--------------
+select count(cand_name) 
+from election
+where partyname='BJP' and st_name='Andaman & Nicobar Islands' and year=1984;
+
+------------query to display male candidates participated in elections in each state---------------
+select st_name,count(cand_sex)
+from election
+where cand_sex='M'
+group by st_name;
+
+------------query to display top 5 parties that got most votes in assam state in the year 2014-----------------
+select partyname
+ from (select rownum slno,partyname
+       from(select distinct PARTYNAME
+            from election
+            where ST_NAME='Assam' and year=2014     
+            order by  partyname desc))
+where slno<6;
