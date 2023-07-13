d2fb118be6c567c907f12d60b4bb9eeae18c4748
harika0412 <harikat660@gmail.com>
2023-07-13 06:51:01 +0000

    Commit

--- Election_table_queries.sql
+++ Election_table_queries.sql
@@ -1,39 +1,54 @@
 -- fetch data from table
 select * from election;
+-- table structure
+desc election;
 
 -------------------------------oracle workspace queries-----------------------------------------
 
 1-- total female candidate participating each year
-SELECT YEAR, COUNT(DISTINCT CAND_NAME) as count
-FROM ELECTION
-WHERE CAND_SEX = 'F'
-GROUP BY YEAR;
+select year, COUNT(CAND_NAME) as count
+from ELECTION
+where CAND_SEX = 'F'
+group by Year;
 
-2--total candidates who participted in election at each state in each year
-select year,ST_NAME,count(distinct cand_name) as count
-from election group by year,ST_NAME;
+2--total candidates who participated in election at each state in each year
+select year,ST_NAME,count(cand_name) as count
+from election
+group by year,ST_NAME;
 
 3--total votes bjp got in each state in year 1987
 select ST_NAME,sum(TOTVOTPOLL) 
 as totalvotes from election where
- PARTYABBRE='BJP' and year=1987 group by
-ST_NAME,TOTVOTPOLL;
+ PARTYABBRE='BJP' and year=1987
+group by ST_NAME;
+--no rows will be fetched by the above query as no data available----------------------
+----changing year to 1989 will give an output--------------------------------
+select ST_NAME,sum(TOTVOTPOLL) 
+as totalvotes from election where
+ PARTYABBRE='BJP' and year=1989
+group by ST_NAME;
 
 4--total candidates who participated in election in each state in year 2004
-select ST_NAME,count(distinct cand_name) as count 
-from election where year=2004 group by ST_NAME;
+select ST_NAME,count(cand_name) as count 
+from election 
+where year=2004
+group by ST_NAME;
 
 5--top5 parties got the most votes in uttarpradesh in year 2014
-SELECT 
-    PARTYNAME FROM (SELECT PARTYNAME, SUM(TOTVOTPOLL)AS totalvotes
-    FROM 
-    ELECTION WHERE ST_NAME = 'uttar Pradesh' AND YEAR = 1977
-    GROUP BY 
-        PARTYNAME
-)
-ORDER BY totalvotes DESC
-FETCH FIRST 5 ROWS ONLY; 
-
+Select PARTYNAME from (select PARTYNAME, sum(TOTVOTPOLL) 
+as totalvotes from ELECTION
+where ST_NAME = 'uttar Pradesh' and  year = 1977
+group by PARTYNAME) 
+order by totalvotes DESC
+FETCH FIRST 5 ROWS ONLY;
+--for the above query as there is no state named uttatpradesh
+--replacing the state name can give us the output
+Select PARTYNAME from (select PARTYNAME, sum(TOTVOTPOLL) 
+as totalvotes from ELECTION
+where ST_NAME = 'Andhra Pradesh' and  year = 1977
+group by PARTYNAME) 
+order by totalvotes DESC
+FETCH FIRST 5 ROWS ONLY;
 
 ------------------------------Example Queries----------------------------------------------
 
@@ -41,91 +56,139 @@ FETCH FIRST 5 ROWS ONLY;
 
 1--totalvotes bjp party got in each state in year 1989
 select sum(TOTVOTPOLL) as totalvotes,ST_NAME as state
-from ELECTION where year=1989 and PARTYABBRE='BJP'
- group by st_name;
+from ELECTION 
+where year=1989 and PARTYABBRE='BJP'
+group by st_name;
 
 2--total candidates who participated from Arunachal Pradesh in year 2014
-select count(distinct CAND_NAME) as candidatecount from election
+select count(distinct CAND_NAME) as candidatecount 
+from election
 where ST_NAME='Arunachal Pradesh' and year=2014;
 
 3--total votes bjp got in state of Andhra Pradesh in year 1989
 
-select sum(TOTVOTPOLL) as totalvotes from ELECTION
+select sum(TOTVOTPOLL) as totalvotes 
+from ELECTION
 where ST_NAME='Andhra Pradesh' and year= 1989
 and partyabbre='BJP';
 
 4--total vote percentage bjp got in state of Andhra Pradesh in 1989
 select (sum(TOTVOTPOLL)/sum(ELECTORS))*100 as votepercentage
-from election where ST_NAME = 'Andhra Pradesh' and year=1989
+from election 
+where ST_NAME = 'Andhra Pradesh' and year=1989
 and PARTYABBRE='BJP';
 
-5-- how many times bjp has gotten above 50% votes
-SELECT COUNT(*)
-FROM (
-  SELECT ST_NAME, YEAR, SUM(TOTVOTPOLL)/(SELECT SUM(TOTVOTPOLL) FROM ELECTION WHERE YEAR = ELECTION.YEAR AND ST_NAME = ELECTION.ST_NAME) * 100 AS 
-  Above_vote_percent
-  FROM ELECTION
-  WHERE PARTYABBRE = 'BJP'
-  GROUP BY ST_NAME, YEAR
-)  SUBQUERY
-WHERE Above_vote_percent> 50;
-
+5-- how many times bjp has gotten below 50% votes
+select count(*) as count 
+from ELECTION
+where PARTYABBRE='BJP'
+and ((TOTVOTPOLL/ELECTORS)*100)<50;
 
 6--state list of bjp where it gets 75% below votes
-select ST_NAME as state from election where
-PARTYABBRE='BJP' and ((totvotpoll/electors)*100)<75;
+select ST_NAME as state 
+from election 
+where PARTYABBRE='BJP' and ((totvotpoll/electors)*100)<75;
+
+7-- state list where bjp got below 60% below vote
+select ST_NAME as state 
+from election 
+where PARTYABBRE='BJP' and ((totvotpoll/electors)*100)<60;
+
+8--total candidates who participated in Arunachal Pradesh in year 2014
+select count(cand_name) as candidate_count
+from election 
+where st_name='Arunachal Pradesh' and year=2014;
+
 
 -----------------------performance tuning set2 queries--------------------------------------------
 
 1--bjp candidate list in andaman&nicobar islands,party='bjp' and year=1984
-select CAND_NAME from election where st_name='Andaman & Nicobar Islands' and 
-PARTYABBRE='BJP' and year= 1984;
+select CAND_NAME as candidatename
+from election 
+where st_name='Andaman & Nicobar islands' and 
+PARTYNAME='BJP' and year= 1984;
 
 2--male candidates participated in elections in each state
-SELECT st_name, COUNT(DISTINCT CAND_NAME) as count
-FROM ELECTION
-WHERE CAND_SEX = 'M'
-GROUP BY st_name;
+select ST_NAME, COUNT(CAND_NAME) as count
+from ELECTION
+where CAND_SEX = 'M'
+group by ST_NAME;
 
 3--select partyname in descending order
-select  distinct partyname from election order by partyname desc;
+select  distinct partyname
+from election
+order by partyname desc;
 
 4--candidates participated in year 1988 in west bengal
-select count(distinct cand_name) from ELECTION where year= 1988
- and st_name='West Bengal';
+select count(cand_name) as count
+from ELECTION 
+where year= 1984
+and st_name='Arunachal Pradesh';
 
 5-- Assam candidate list each year
-select year,cand_name from election where 
-st_name='Assam';
+select year,cand_name as candidatename from election 
+where st_name='Assam';
 
+6-- TamilNadu candidate lsit each year
+select year,cand_name as candidatename from election 
+where st_name='TamilNadu';
 
 -------------------------performance tuning set-3 Queries--------------------------------------
 
 1--total male candidates participated in election each year
 
-select year,count(cand_name) from election where cand_sex='M'
+select year,count(cand_name) as candidatename 
+from election
+where cand_sex='M'
 group by year;
 
-2--Sikkim Candidate list each year
-select year,cand_name from election where 
-st_name='Andhra Pradesh';
+2--Andhra Pradesh Candidate list each year
+select year,cand_name as candidatename
+from election 
+where st_name='Andhra Pradesh';
 
 3-- count of how many times congress got above 50% votes in bihar state
-
-select distinct(PARTYABBRE) from election;
+select count(*) as count 
+from ELECTION
+where PARTYABBRE='INC'
+and st_name='Bihar'and ((TOTVOTPOLL/ELECTORS)*100)<50;
 
 4--bjp candidate list in descending order
-select cand_name from election where partyabbre='BJP'
+select cand_name as candidatename
+from election 
+where partyabbre='BJP'
 order by cand_name desc;
 
 5--total votes bjp got in bihar state in 1996
-select sum(TOTVOTPOLL) as totalvotes from election where PARTYABBRE='BJP'
-and year=1996;
+select sum(TOTVOTPOLL) as totalvotes
+from election 
+where PARTYABBRE='BJP'and year=1996;
 
+-------------basic queries on election table--------------------------------------------
+1--state names from election table
+select distinct(st_name) as state
+from election;
 
+2--total parties participated in elections
+select distinct(partyname) from election;
 
+3--count of how many times elections were held
+select count(distinct year)as count 
+from ELECTION;
 
+4--constituency names in elections
+select distinct(PC_NAME) from Election;
 
+5--Candidate type in elections
+select distinct(PC_TYPE) from election;
+
+6--abbrevations of parties participating in elections
+select distinct(PARTYABBRE) from election;
+
+7-- total count of independent party candidates from election
+select count(cand_name) as candidatename
+from election
+where partyname='Independents';
 
 
 
